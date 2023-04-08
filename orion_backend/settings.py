2a7b93ef30c714f62ff5b5516b020dab32eb3a38
Anuragchandra221 <pothaluranurag2002@gmail.com>
2023-04-08 14:06:14 +0530

    updated settings.py

--- orion_backend/settings.py
+++ orion_backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p!4u28=31@qz5md6%5ah9h_m#w*l_s%$00-42wjsre(!h)-_6s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
