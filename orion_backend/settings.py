b7545aaa546b46128af20b597d4a9281cc73a2aa
Anuragchandra221 <pothaluranurag2002@gmail.com>
2023-05-22 23:16:10 +0530

    Server changed to localhost

--- orion_backend/settings.py
+++ orion_backend/settings.py
@@ -144,12 +144,8 @@ WSGI_APPLICATION = 'orion_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '3clMBUa1yP4BbauI721R',
-        'HOST': 'containers-us-west-66.railway.app',
-        'PORT': '6282'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
